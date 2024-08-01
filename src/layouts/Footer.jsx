80d24f6db845245fbd3803f1db52cd8cd0e26c04
5u00n <surenhembram97@gmail.com>
2024-08-01 23:48:13 +0530

    chore: Update page title and remove unused dependencies

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,27 +1,9 @@
-import { Container, Group, ActionIcon, rem } from '@mantine/core';
-import { Image } from '@mantine/core';
-import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
-import { MantineLogo } from '@mantinex/mantine-logo';
-import classes from './FooterSocial.module.css';
+import React from 'react'
 
-export default function Footer() {
+function Footer() {
   return (
-    <div className={classes.footer}>
-      <Container className={classes.inner}>
-        <div></div>
-
-        <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-        </Group>
-      </Container>
-    </div>
-  );
+    <div>Footer</div>
+  )
 }
+
+export default Footer;
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,76 +1,9 @@
-import { Menu, Group, Center, Burger, Container,Image } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { IconChevronDown } from '@tabler/icons-react';
-import { MantineLogo } from '@mantinex/mantine-logo';
-import classes from './HeaderMenu.module.css';
+import React from 'react'
 
-import Logo from '../assets/cropped-3acm.png';
-
-
-const links = [
-  { link: '/', label: 'Home' },
-  {
-    link: '/about',
-    label: 'Adout Us',
-  },
-  { link: '/projects', label: 'Projects' },
-  {
-    link: '/mission-trips',
-    label: 'Mission Trips',
-  },
-];
-
-export default function Header() {
-  const [opened, { toggle }] = useDisclosure(false);
-
-  const items = links.map((link) => {
-    const menuItems = link.links?.map((item) => (
-      <Menu.Item key={item.link}>{item.label}</Menu.Item>
-    ));
-
-    if (menuItems) {
-      return (
-        <Menu key={link.label} trigger="hover" transitionProps={{ exitDuration: 0 }} withinPortal>
-          <Menu.Target>
-            <a
-              href={link.link}
-              className={classes.link}
-              onClick={(event) => event.preventDefault()}
-            >
-              <Center>
-                <span className={classes.linkLabel}>{link.label}</span>
-                <IconChevronDown size="0.9rem" stroke={1.5} />
-              </Center>
-            </a>
-          </Menu.Target>
-          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
-        </Menu>
-      );
-    }
-
-    return (
-      <a
-        key={link.label}
-        href={link.link}
-        className={classes.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </a>
-    );
-  });
-
+function Header() {
   return (
-    <header className={classes.header}>
-      <Container size="md">
-        <div className={classes.inner}>
-          <Image src={Logo} alt="Unsplash"  style={{width:'54px'}} />
-          <Group gap={5} visibleFrom="sm">
-            {items}
-          </Group>
-          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
-        </div>
-      </Container>
-    </header>
-  );
+    <div>Header</div>
+  )
 }
+
+export default Header;
